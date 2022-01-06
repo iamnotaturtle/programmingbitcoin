f7933e1423c21c22a2f44e74ba52f1b186dede3e
Yakov <iamnotaturtle@users.noreply.github.com>
2022-01-06 08:43:48 -0500

    done

--- code-ch12/network.py
+++ code-ch12/network.py
@@ -313,10 +313,14 @@ class GetDataMessage:
 
     def serialize(self):
         # start with the number of items as a varint
+        s = encode_varint(len(self.data))
         # loop through each tuple (data_type, identifier) in self.data
+        for tup in self.data:
             # data type is 4 bytes Little-Endian
+            s += int_to_little_endian(tup[0], 4)
             # identifier needs to be in Little-Endian
-        raise NotImplementedError
+            s += tup[1][::-1]
+        return s
 
 
 class GetDataMessageTest(TestCase):
